d6a833831d8453970ace29d24c2fb035fd16bed8
Jubilio <jubiliomausse@outlook.pt>
2025-05-30 20:34:59 +0200

    feat: first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,18 +5,24 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Jason Wells - Web Developer",
-  description: "Portfolio website of Jason Wells, a Senior Web Developer",
+  title: "Jubilio Mausse - Backend Developer and GIS Analyst",
+  description: "Portfolio website of Jubilio Mausse, a Backend Developer",
 };
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  readonly children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased bg-black text-white`}>
+        <div className="fixed inset-0 w-full h-full z-10">
+          <video autoPlay loop muted className="w-full h-full object-cover object-center">
+            <source src="/bgvid.mp4" type="video/mp4" />
+          </video>
+          <div className="absolute inset-0 backdrop-blur-md"></div>
+        </div>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,117 @@
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import {
+  BiBuilding,
+  BiChevronRight,
+  BiLogoGithub,
+  BiLogoYoutube,
+  BiLogoLinkedin,
+} from 'react-icons/bi';
+
+import { HiArrowDown } from 'react-icons/hi';
+
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      Portfolio Website Starter Code
+      <main className="w-full max-w-xl m-3 md:m-8 z-50 space-y-8 border border-gray-700/50
+                      rounded-xl p-5 sm:p-10 backdrop-blur-xl bg-black/80">
+
+        <div className="flex sm:flex-row sm:justify-between sm:items-center flex-col-reverse 
+                        items-start gap-3 sm:gap-10">
+
+          <div className="flex items-center gap-3">
+            <BiBuilding className="w-5 h-5" />
+            <h3 className='text-gray-400'>Pemba City, Cabo Delgado, Moz</h3>
+          </div>
+          <Button variant="ghost" className='hover:bg-black/50 duration-300
+            hover:text-white rounded-full'>
+            <div className='relative flex text-sm gap-1 items-center justify-center
+                              h-3 w-3 rounded-full'>
+              <span className='animate-ping absolute inline-flex h-full w-full rounded-full bg-green-500'></span>
+              <span className='inline-flex rounded-full h-3 w-3 bg-green-500'></span>
+            </div>
+            <p>Available For Work</p>
+          </Button>
+        </div>
+
+        <div>
+          <h1 className="text-4xl">Jubilio Mausse</h1>
+          <h3 className="text-xl font-bold mt-2 bg-clip-text text-transparent bg-gradient-to-r 
+                        from-emerald-400 via-sky-300 to-gray-400">Data and GIS Analyst</h3>
+          <p className="text-pretty tracking-tight mt-3 text-neutral-300">
+            I am a Data and GIS Analyst with a passion for transforming data into actionable insights.
+            I specialize in Geographic Information Systems (GIS) and data analysis, helping organizations
+            make informed decisions based on spatial data. My expertise includes data visualization,
+            spatial analysis, and creating interactive maps to communicate complex information effectively.{" "}
+            <Link href="/contact" className="bg-clip-text text-transparent bg-gradient-to-r 
+                        from-emerald-400 via-sky-300 to-blue-500 font-bold opacity-90 hover:opacity-100 transition-opacity">
+                          Contact Me{" "}
+            </Link>{" "}
+            for freelance opportunities or to discuss how I can help your organization leverage the power of data.
+          </p>
+        </div>
+
+        <nav className='space-y-4'>
+          
+          <h3 className='text-xl font-bold text-emerald-400'>Menu</h3>
+          
+          <div className='flex flex-col gap-3'>
+            <Link href="/about" className='w-1/2 border bg-black/30 border-gray-700/40 flex group items-center justify-between
+                                          p-3 cursor-pointer transition-colors duration-200 ease-linear hover:bg-zinc-800/40 rounded-lg'>
+              <span>About</span>
+              <BiChevronRight className='mr-5 text-2xl translate-x-0 group-hover:translate-x-3 text-gray-300 
+                                          opacity-0 group-hover:opacity-100 transition-all duration-200' ></BiChevronRight>
+            </Link>
+
+            <Link href="/work" className='w-1/2 border bg-black/30 border-gray-700/40 flex group items-center justify-between
+                                          p-3 cursor-pointer transition-colors duration-200 ease-linear hover:bg-zinc-800/40 rounded-lg'>
+              <span>Work</span>
+              <BiChevronRight className='mr-5 text-2xl translate-x-0 group-hover:translate-x-3 text-gray-300 
+                                          opacity-0 group-hover:opacity-100 transition-all duration-200' ></BiChevronRight>
+            </Link>
+
+            <Link href="/skills" className='w-1/2 border bg-black/30 border-gray-700/40 flex group items-center justify-between
+                                          p-3 cursor-pointer transition-colors duration-200 ease-linear hover:bg-zinc-800/40 rounded-lg'>
+              <span>Skills</span>
+              <BiChevronRight className='mr-5 text-2xl translate-x-0 group-hover:translate-x-3 text-gray-300 
+                                          opacity-0 group-hover:opacity-100 transition-all duration-200' ></BiChevronRight>
+            </Link>
+
+            <Link href="/contact" className='w-1/2 border bg-black/30 border-gray-700/40 flex group items-center justify-between
+                                          p-3 cursor-pointer transition-colors duration-200 ease-linear hover:bg-zinc-800/40 rounded-lg'>
+              <span>Contact</span>
+              <BiChevronRight className='mr-5 text-2xl translate-x-0 group-hover:translate-x-3 text-gray-300 
+                                          opacity-0 group-hover:opacity-100 transition-all duration-200' ></BiChevronRight>
+            </Link>
+
+          </div>
+
+          <div className='flex gap-2 items-center'>
+            <h3 className='text-xl bg-clip-text font-bold text-transparent bg-gradient-to-r from-emerald-400
+                            via-sky-400 to-blue-400'>Check out my socials</h3>
+            <HiArrowDown className='text-2xl text-sky-400' ></HiArrowDown>
+          </div>
+
+          <div className='flex flex-col gap-3'>
+            <Button className='bg-black/30 w-full p-6 text-gray-300 border border-gray-700/40 hover:bg-slate-300/20
+                                hover:text-white transition-colors duration-200'>
+              <BiLogoGithub/> GitHub
+              <Link href="https://github.com/Jubilio/"></Link>
+            </Button>
+
+            <Button className='bg-black/30 w-full p-6 text-gray-300 border border-gray-700/40 hover:bg-slate-300/20
+                                hover:text-white transition-colors duration-200'>
+              <BiLogoLinkedin/> LinkedIn
+            </Button>
+
+            <Button className='bg-black/30 w-full p-6 text-gray-300 border border-gray-700/40 hover:bg-slate-300/20
+                                hover:text-white transition-colors duration-200'>
+              <BiLogoYoutube/> YouTube
+            </Button>
+          </div>
+        </nav>
+
+      </main>
     </div>
   );
 }
