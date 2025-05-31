64d797261795845852b72c602083e9624d0def60
Jubilio <jubiliomausse@outlook.pt>
2025-05-31 19:17:16 +0200

    new changes: page about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -20,9 +20,18 @@ const page = () => {
 
                     <div className="space-y-4 text-neutral-200 text-pretty tracking-tight">
                         <p className="text-gray-300">
-                            Hello! I'm a passionate web developer with a love for creating dynamic and responsive web applications.
-                            I enjoy working with the latest technologies and continuously learning new skills to enhance my craft.
+                            I am a GIS and data analysis specialist with a degree in Geographic Information Sciences from Eduardo Mondlane University.
+                            I currently work as a GIS Officer at Acted, contributing to the Rapid Response Mechanism project, where I support the integration
+                            of geospatial and humanitarian data for emergency responses.
+                            I use tools such as KoboToolbox, R Framework, ArcGIS Pro and Power BI to transform complex data into actionable analysis.
                         </p>
+                        <p className="text-gray-300">
+                            My experience includes leading teams and developing impactful visualizations for humanitarian reports.
+                            I value data-driven solutions to improve crisis coordination and optimize response strategies.
+                            I continually seek to leverage geospatial technologies and analytical methodologies to support
+                            effective and informed humanitarian decisions.
+                        </p>
+                        
                         <p className="text-gray-300">
                             In my free time, I like to explore new frameworks, contribute to open-source projects, and share my knowledge with the community.
                         </p>
@@ -37,7 +46,7 @@ const page = () => {
                         </div>
                     </div>
 
-                     <div className="space-y-4">
+                    <div className="space-y-4">
                         <h2 className="text-4xl bg-clip-text font-bold text-transparent bg-gradient-to-r
                                 from-emerald-400 to-blue-500 text-center">Profissional Experience</h2>
                         <div className="space-y-2">
